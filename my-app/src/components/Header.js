4360e149a01ac8e56a7ad22d44652c6232eea215
Your Name <you@example.com>
2023-02-28 11:46:41 +0000

    header start

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,40 @@
+
+function Header() {
+    return (
+      <div className="Header">
+
+
+      <div className="header-1st-line">
+        <div className="logo">
+          
+          <div className="logo-img">
+            <div className="logo-img-container"></div>
+            </div>
+
+            <div className="logo-text">logo text</div>
+          
+          </div>
+
+          <div className="links">
+            <ul>
+              <li>Events</li>
+              <li>Games</li>
+              <li>Badges</li>
+              
+              
+              
+              
+            </ul></div>
+
+          <div className="login">Login/register</div>
+
+        
+        </div>
+      
+
+
+      </div>
+    );
+  }
+  
+  export default Header;
---
+++ my-app/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+
+function Home() {
+  return (
+    <div className="Home">
+    <p>sdflogfdlgmdfg</p>
+    <div className="h">
+    <h1>fgbhgfhfg</h1>
+    </div>
+    </div>
+  );
+}
+
+export default Home;
