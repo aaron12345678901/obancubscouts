d632c6abebc629cf38a0d8f423e6ac9af25e6161
Your Name <you@example.com>
2023-03-21 11:32:31 +0000

    badges page update

--- my-app/src/pages/Badges.jsx
+++ my-app/src/pages/Badges.jsx
@@ -1,36 +1,114 @@
 import React from "react";
 
-import badge1 from '../images/Badges/badge1.png'
+import badge1 from "../images/Badges/badge1.png";
+import badge2 from "../images/Badges/badge2.png";
+import badge3 from "../images/Badges/badge3.png";
+import badge4 from "../images/Badges/badge4.png";
+import badge5 from "../images/Badges/badge5.png";
+import badge6 from "../images/Badges/badge6.png";
+import badge7 from "../images/Badges/badge7.png";
+import badge8 from "../images/Badges/badge8.png";
+import badge9 from "../images/Badges/badge9.png";
+import badge10 from "../images/Badges/badge10.png";
+import badge11 from "../images/Badges/badge11.png";
+import badge12 from "../images/Badges/badge12.png";
+import badge13 from "../images/Badges/badge13.png";
+import badge14 from "../images/Badges/badge14.png";
+import badge15 from "../images/Badges/badge15.png";
 
 function Badges() {
+  return (
+    <div className="Badges-main">
+      <div className="badges-heading">
+        <h1>scouts activity badges </h1>
+      </div>
 
-  
-
-    return (
-      <div className="Badges-main">
-       <div className="badges-heading"><h1>scouts activity badges </h1></div>
-
-       <div className="first-badges">
-
-
+      <div className="first-badges">
         <div className="badge1">
-            <img src={badge1} alt="" />
-            <p>fdsbgfdb</p>
+          <img src={badge1} alt="" />
+          <p id="badgep">Artist</p>
         </div>
 
         <div className="badge2">
-        <div className="badge-img-2"></div>
-
+          <img src={badge2} alt="" />
+          <p id="badgep">Air Researcher</p>
         </div>
+
         <div className="badge3">
-        <div className="badge-img-3"></div>
-
+          <img src={badge3} alt="" />
+          <p id="badgep">bush</p>
         </div>
-       </div>
-
       </div>
-    );
-  }
-  
-  export default Badges;
-  
+
+      <div className="second-badges">
+        <div className="badge4">
+          <img src={badge4} alt="" />
+          <p id="badgep">hobbies</p>
+        </div>
+
+        <div className="badge5">
+          <img src={badge5} alt="" />
+          <p id="badgep">community aid </p>
+        </div>
+
+        <div className="badge6">
+          <img src={badge6} alt="" />
+          <p id="badgep">instructor</p>
+        </div>
+      </div>
+
+      <div className="third-badges">
+        <div className="badge7">
+          <img src={badge7} alt="" />
+          <p id="badgep">Nautical Skills</p>
+        </div>
+
+        <div className="badge8">
+          <img src={badge8} alt="" />
+          <p id="badgep">craft</p>
+        </div>
+
+        <div className="badge9">
+          <img src={badge9} alt="" />
+          <p id="badgep"> Seat Rowing</p>
+        </div>
+      </div>
+
+      <div className="fourth-badges">
+        <div className="badge10">
+          <img src={badge10} alt="" />
+          <p id="badgep">fund raising</p>
+        </div>
+
+        <div className="badge11">
+          <img src={badge11} alt="" />
+          <p id="badgep">Air Activities</p>
+        </div>
+
+        <div className="badge12">
+          <img src={badge12} alt="" />
+          <p id="badgep">Orienteer</p>
+        </div>
+      </div>
+
+      <div className="fith-badges">
+        <div className="badge13">
+          <img src={badge13} alt="" />
+          <p id="badgep"> Conservation</p>
+        </div>
+
+        <div className="badge14">
+          <img src={badge14} alt="" />
+          <p id="badgep">Community Impact</p>
+        </div>
+
+        <div className="badge15">
+          <img src={badge15} alt="" />
+          <p id="badgep">camping</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Badges;
