1975e010b8f9c431ad1176a5b048521bc64c3088
Your Name <you@example.com>
2023-04-18 15:37:20 +0100

    aduilt profile ui done

--- my-app/src/pages/Aduiltpages/Aduiltprofile.jsx
+++ my-app/src/pages/Aduiltpages/Aduiltprofile.jsx
@@ -1,10 +1,73 @@
+import placeholder from "../../images/profilepics/placeholderpic.png";
+import tick from "../../images/tick.png"
+
 function Aduiltprofile() {
-    return (
-      <div className="Aduiltprofile-main">
-  <p>fdgfdg</p>
+  return (
+    <div className="Aduiltprofile-main">
+      <div className="A-profile-align">
+        <h1>my profile </h1>
+        <p>set your name bio and other public information </p>
+
+        <div className="profile-head-elements">
+          <h2>Profile</h2>
+        </div>
+
+        <div className="A-profile-card">
+          <div className="A-inner-profile-card">
+
+
+          <div className="profilepic-and-names">
+            <div className="Aprofilepic">
+              <img src={placeholder} alt="" />
+              <p>click to change profile picture </p>
+            </div>
+
+            <div className="A-names">
+              <form action="">
+                <input
+                  type="text"
+                  id="Afname"
+                  name="Afname"
+                  value="first name:"
+                ></input>{" "}
+                <br />
+                <input
+                  type="text"
+                  id="Asname"
+                  name="Asname"
+                  value="surname:"
+                ></input>
+              </form>
+            </div>
+          </div>
+          
+          <div className="A-aboutme">
+            <form action="">
+            <input
+                  type="text"
+                  id="A-about"
+                  name="A-about"
+                  value="about me:"
+                ></input>
+            </form>
+          </div>
+
+          <div className="A-passed-course">
+            <ul>
+              <li>disclosure:   <img src={tick} alt="" />  </li>
+              <li id="no-underline">training attended</li>
+              <li>first aid level 3</li>
+              <li>group leader </li>
+              <li>water safety</li>
+              <li>intro to climbing </li>
+            </ul>
+          </div>
+
+          </div>
+        </div>
       </div>
-    );
-  }
-  
-  export default Aduiltprofile;
-  
+    </div>
+  );
+}
+
+export default Aduiltprofile;
